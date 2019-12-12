44e00e9077be1b3c0a8790f6713f916eadb70e7c
Oscar <oscar.cortes@upf.edu>
2019-12-12 09:56:01 +0100

    carpetes de treball

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,17 @@
+/**
+ * Import dependencies from node_modules
+ * see commented examples below
+ */
+
+// import 'some-node-module';
+// import SomeModule from 'some-node-module';
+
+import 'bootstrap';
+
+/**
+ * Write any other JavaScript below
+ */
+
++( function() {
+  console.log('Hello, UOC!');
+} )();
